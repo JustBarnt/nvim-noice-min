692109738c84ebd669e3db7051b26005554abc5b
Brent Williams <functnal@gmail.com>
2024-09-18 11:54:06 -0400

    initial

---
+++ .repro/state/nvim-noice-min-data/lazy/pkg-cache.lua
@@ -0,0 +1,10 @@
+return {pkgs={{name="noice.nvim",file="lazy.lua",source="lazy",spec=function()
+return {
+  -- nui.nvim can be lazy loaded
+  { "MunifTanjim/nui.nvim", lazy = true },
+  {
+    "folke/noice.nvim",
+  },
+}
+
+end,dir="C:/Users/bwilliams/AppData/Local/nvim-noice-min/.repro/plugins/noice.nvim",},},version=12,}
---
+++ init.lua
@@ -0,0 +1,66 @@
+local root = vim.fn.fnamemodify("./.repro", ":p")
+
+-- set stdpaths to use .repro
+for _, name in ipairs({ "config", "data", "state", "cache" }) do
+	vim.env[("XDG_%s_HOME"):format(name:upper())] = root .. "/" .. name
+end
+
+-- bootstrap lazy
+local lazypath = root .. "/plugins/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--single-branch",
+		"https://github.com/folke/lazy.nvim.git",
+		lazypath,
+	})
+end
+vim.opt.runtimepath:prepend(lazypath)
+
+-- install plugins
+local plugins = {
+	-- do not remove the colorscheme!
+	"folke/tokyonight.nvim",
+	-- add any other pugins here
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			lsp = {
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true,
+				},
+			},
+			routes = {
+				{
+					filter = {
+						event = "msg_show",
+						any = {
+							{ find = "%d+L, %d+B" },
+							{ find = "; after #%d+" },
+							{ find = "; before #%d+" },
+						},
+					},
+					view = "mini",
+				},
+			},
+			presets = {
+				bottom_search = true,
+				command_palette = true,
+				long_message_to_split = true,
+			},
+		}
+	},
+}
+require("lazy").setup(plugins, {
+	root = root .. "/plugins",
+})
+
+-- add anything else here
+vim.opt.termguicolors = true
+-- do not remove the colorscheme!
+vim.cmd([[colorscheme tokyonight]])
